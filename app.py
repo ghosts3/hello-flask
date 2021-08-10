6b2718768c9a29b8f7f42a34a1865ded79716270
ghosts3 <emailforweb@gmail.com>
2021-08-10 00:03:58 -0600

    fix main template

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home(message=None):
-  message = "Huzzah. Your flask template is working."
+  message = "Huzzah. Your n00b flask app is working."
   return render_template("main.html",message=message)
